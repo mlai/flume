3d2b1a5329fbe27a357903cbdfb7f261bf6b07f2
Mingjie Lai <mingjie_lai@trendmicro.com>
2011-07-29 02:21:34 -0700

    Sink decorators for hbase

--- plugins/flume-plugin-jruby/scripts/FullLogAgentDecorator.rb
+++ plugins/flume-plugin-jruby/scripts/FullLogAgentDecorator.rb
@@ -4,6 +4,9 @@ java_import 'com.cloudera.flume.core.EventSinkDecorator'
 java_import 'com.cloudera.flume.core.Event'
 java_import 'com.cloudera.flume.core.EventImpl'
 
+java_import 'org.slf4j.Logger'
+java_import 'org.slf4j.LoggerFactory'
+
 # Full log decorator.
 # 
 # Fields:
@@ -14,19 +17,45 @@ java_import 'com.cloudera.flume.core.EventImpl'
 #
 # It filters some specified fields from the passed events: agent ip.
 class FullLogAgentDecorator < EventSinkDecorator
+  PATTERN  = /^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\t([a-zA-Z0-9]+)\t([0-9]+)\t([0-9, ]*)\t(.*)/
+  # @@log = LoggerFactory.getLogger("FullLogAgentDecorator")
+  
+  def getReverseDomainName(domainName)
+    ret = ""
+    substring = domainName.split(".")
+    substring = substring.reverse
+    substring.each_with_index { |y, x| 
+      ret = (x == substring.size - 1) ? (ret + y.to_s) : (ret + y.to_s + ".")
+    }
+    ret
+  end
+  
+  def getDateTimeByTimeStamp(ts)
+    t = Time.at(ts)
+    return t.year.to_s + "%02d" % t.month.to_s + "%02d" % t.day.to_s + 
+      "%02d" % t.hour.to_s + "%02d" % t.min.to_s + "%02d" % t.sec.to_s
+  end
+  
   def append(e)
-    pattern = /^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\t([a-zA-Z0-9])+\t([0-9])+\t(\d{1,3})*\t(.)*/
-    
     body = String.from_java_bytes e.getBody
-    newBody = ""
-    if (pattern.match(body)) 
+    if (PATTERN .match(body)) 
       substrings = body.split(/\t/)
-      newBody = substrings[1] + "\t" + substrings[2] + "\t" + substrings[3] +
-        "\t" + substrings[4]
-      super EventImpl.new(newBody.to_java_bytes)
+      
+      newEvent = EventImpl.new("".to_java_bytes)
+      newEvent.set("lic", substrings[1].to_java_bytes)
+      newEvent.set("ts", substrings[2].to_java_bytes)
+      newEvent.set("rating", substrings[3].to_java_bytes)
+      
+      urlsubstring = substrings[4].split("/")
+      
+      newEvent.set("url", substrings[4].to_java_bytes)
+      newEvent.set("key", (getDateTimeByTimeStamp(substrings[2].to_i) + ":" + 
+        getReverseDomainName(urlsubstring[0].split(":")[0])).to_java_bytes)
+      super newEvent
+    else
+      super e  
     end
   end
 end
 
 FullLogAgentDecorator.new(nil)
-
