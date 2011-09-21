23a44ef085889401578fcb0722c72ff2f51b14e1
Mingjie Lai <mingjie_lai@trendmicro.com>
2011-09-21 16:40:36 -0700

    Seeded full log decorator.

---
+++ plugins/flume-plugin-jruby/scripts/SeededFullLogDecorator.rb
@@ -0,0 +1,62 @@
+# FullLogDecorator.rb
+require 'java'
+java_import 'com.cloudera.flume.core.EventSinkDecorator'
+java_import 'com.cloudera.flume.core.Event'
+java_import 'com.cloudera.flume.core.EventImpl'
+
+java_import 'org.slf4j.Logger'
+java_import 'org.slf4j.LoggerFactory'
+
+# Full log decorator: changed the timestamp to use current time. 
+# This decorator is for performance test purpose. 
+class SeedededFullLogDecorator < EventSinkDecorator
+  
+  @@log = LoggerFactory.getLogger("SeedededFullLogDecorator")
+  
+  def getReverseDomainName(domainName)
+    ret = ""
+    substring = domainName.split(".")
+    substring = substring.reverse
+    substring.each_with_index { |y, x| 
+      ret = (x == substring.size - 1) ? (ret + y.to_s) : (ret + y.to_s + ".")
+    }
+    ret
+  end
+  
+  def getDateTimeByTimeStamp(ts)
+    t = Time.at(ts)
+    return t.year.to_s + "%02d" % t.month.to_s + "%02d" % t.day.to_s + 
+      "%02d" % t.hour.to_s + "%02d" % t.min.to_s + "%02d" % t.sec.to_s
+  end
+  
+  def append(e)
+    begin
+      body = String.from_java_bytes e.getBody
+      pattern  = /^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\t([a-zA-Z0-9]+)\t([0-9]+)\t([0-9, ]*)\t(.*)$/
+      if (pattern.match(body)) 
+        substrings = body.split(/\t/)
+        
+        newEvent = EventImpl.new("".to_java_bytes)
+        newEvent.set("lic", substrings[1].to_java_bytes)
+        newTS = Time.at(Time.now()) - Time.at(0)
+        newEvent.set("ts", newTS.to_s.to_java_bytes)
+        newEvent.set("rt", substrings[3].to_java_bytes)
+        
+        urlsubstring = substrings[4].split("/")
+        
+        newEvent.set("url", substrings[4].to_java_bytes)
+        newEvent.set("key", ( "%02d" % (rand(4) + 1).to_s + ":" + 
+          getDateTimeByTimeStamp(newTS)  + ":" +
+          getReverseDomainName(urlsubstring[0].split(":")[0])).to_java_bytes)
+        super newEvent
+      else
+        @@log.info("++++++")
+        super e
+      end
+    rescue
+      super e
+    end
+  end
+end
+
+SeedededFullLogDecorator.new(nil)
